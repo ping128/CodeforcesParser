209289ce8d2f92817c8ab1e845540e3373518499
ping128 <sping128@gmail.com>
2013-11-06 17:19:11 -0800

    parsing input and output

--- src/Problem.java
+++ src/Problem.java
@@ -1,9 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Problem {
-	
+
 	public String name;
-	
-	public Problem(){
-		
+	public List<String> inputs;
+	public List<String> outputs;
+
+	public Problem(String name) {
+		this.name = name;
+		this.inputs = new ArrayList<String>();
+		this.outputs = new ArrayList<String>();
+	}
+
+	public String toString() {
+		String ret = "";
+		ret += name + "\n";
+		return ret;
+	}
+
+	public void addSampleTest(String input, String output) {
+		this.inputs.add(parse(input));
+		this.outputs.add(parse(output));
+		System.out.println(input);
+	}
+
+	public String parse(String text) {
+		StringBuilder ret = new StringBuilder();
+		Scanner cin = new Scanner(text);
+		System.out.println(text.length());
+		String[] s2 = text.split("\n|\r\n");
+		for (String s : s2) {
+			s = s.trim();
+			if (s.length() > 0) {
+				ret.append(s + "\n");
+				break;
+			}
+		}
+		return ret.toString();
+	}
+
+	public int numSampleTest() {
+		return this.inputs.size();
 	}
 }
--- src/WebCrawler.java
+++ src/WebCrawler.java
@@ -7,37 +7,38 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
-
 public class WebCrawler {
-	
+
 	private String username;
 	private String password;
 	private WebDriver crawler;
-	
-	public WebCrawler(String username, String password){
+
+	public WebCrawler(String username, String password) {
 		crawler = null;
 		this.username = username;
 		this.password = password;
 	}
-	
-	public String getUsername(){
+
+	public String getUsername() {
 		return username;
 	}
-	
-	public String getPassword(){
+
+	public String getPassword() {
 		return password;
 	}
-	
-	public WebDriver getCrawler(){
+
+	public WebDriver getCrawler() {
 		return crawler;
 	}
-	
-	public String getLocation(){
-		if(crawler == null) return "Offline";
-		else return crawler.getCurrentUrl();
+
+	public String getLocation() {
+		if (crawler == null)
+			return "Offline";
+		else
+			return crawler.getCurrentUrl();
 	}
-	
-	public void parseContest(String contestURL){
+
+	public void parseContest(String contestURL) {
 		System.out.println("Parsing...\n");
 		crawler.get(contestURL);
 		Document doc = Jsoup.parse(crawler.getPageSource());
@@ -45,14 +46,23 @@ public class WebCrawler {
 		System.out.println(crawler.getTitle());
 		int nProblem = problems.size() - 1;
 		System.out.println("There are " + nProblem + " problems.");
-		for(int i = 1; i <= nProblem; i++ ){
-			String problemURL = "http://codeforces.com/" + problems.get(i).select("a").first().attr("href");
+		for (int i = 1; i <= nProblem; i++) {
+			String problemURL = "http://codeforces.com/"
+					+ problems.get(i).select("a").first().attr("href");
 			crawler.get(problemURL);
 			System.out.println(crawler.getTitle());
+			doc = Jsoup.parse(crawler.getPageSource());
+			Elements temp = doc.getElementsByClass("title");
+			Problem problem = new Problem(temp.first().text());
+			Elements inputs = doc.select(".input pre");
+			Elements outputs = doc.select(".output pre");
+			for (int j = 0; j < inputs.size(); j++) {
+				problem.addSampleTest(inputs.get(j).text(), outputs.get(j).text());
+			}
 		}
 	}
-	
-	public void login(String url){
+
+	public void login(String url) {
 		System.out.println("logging in to " + url + "\n");
 		crawler = new HtmlUnitDriver(false);
 		crawler.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
@@ -64,11 +74,11 @@ public class WebCrawler {
 		crawler.findElement(By.className("submit")).click();
 		System.out.println("Login Finished\n");
 	}
-	
+
 	@Override
-	public String toString(){
+	public String toString() {
 		String s = "Crawler: " + username + "\n";
-		if(crawler == null){
+		if (crawler == null) {
 			s += "Offline";
 		} else {
 			s += "Online: " + crawler.getCurrentUrl() + "\n";
