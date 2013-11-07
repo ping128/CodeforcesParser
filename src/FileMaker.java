dafc080c6eae2091bfac21571df7352342aab4d1
ping128 <sping128@gmail.com>
2013-11-06 17:55:16 -0800

    finish parsing input/output

---
+++ src/FileMaker.java
@@ -0,0 +1,12 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+
+public class FileMaker {
+
+	public static void buildFile(String fileName, String text) throws IOException{
+		FileOutputStream out = new FileOutputStream(fileName);
+		out.write(text.getBytes());
+		out.close();
+	}
+}
